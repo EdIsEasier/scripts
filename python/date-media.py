a2366b4a0b24082de13d561eb7ab819e5998f8d7
Edwin Kruglov <ekruglovas@gmail.com>
2018-06-28 14:58:51 +0100

    Add argument validation

--- python/date-media.py
+++ python/date-media.py
@@ -1,2 +1,13 @@
+import sys
+import os
 from dependencies import exiftool
 
+if len(sys.argv) < 2:
+    print("No directory argument provided!", file=sys.stderr)
+    sys.exit(1)
+elif not os.path.isdir(sys.argv[1]):
+    print("Supplied path does not exist!", file=sys.stderr)
+    sys.exit(1)
+else:
+    print("Yay!")
+    sys.exit(0)
